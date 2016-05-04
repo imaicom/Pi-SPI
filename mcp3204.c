5059fdabca9baf517589c3e08d6ae6001a900bb6
imaicom <i@okako.com>
2016-05-04 13:24:05 +0900

    Update mcp3204.c

--- mcp3204.c
+++ mcp3204.c
@@ -10,17 +10,17 @@ int main () {
   unsigned char data[3];
   
   wiringPiSetup();
-  wiringPiSPISetup(0,1000000);
+  wiringPiSPISetup(0 , 1000000);
   
   printf("Raspberry Pi - SPI Analog Test\n");
   
   while(1) {
-    data[0]=0x06;
-    data[1]=0x3F;
-    data[2]=0xFF;
+    data[0] = 0x06;
+    data[1] = 0x3F;
+    data[2] = 0xFF;
     
     wiringPiSPIDataRW(0,data,3);
-    value=((int)data[1]<<8)+(int)data[2];
+    value=((int)data[1]<<8) + (int)data[2];
     vIn=value * 3.3 / 4095.0;
     printf("Voltage = %2.2f\n",vIn);
     delay(500);
